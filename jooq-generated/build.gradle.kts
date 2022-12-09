d7548f7ba66f5c5182e2a282bebb17065a59d2ef
mistresssfilth <115894146+mistresssfilth@users.noreply.github.com>
2022-12-09 19:49:18 +0300

    Add files via upload

---
+++ jooq-generated/build.gradle.kts
@@ -0,0 +1,40 @@
+dependencies {
+    jooqGenerator("org.postgresql:postgresql:42.5.0")
+}
+jooq {
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(false)
+
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/jettyProject"
+                    user = "postgres"
+                    password = "admin"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "generated"
+                        directory = "src/main/java"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
